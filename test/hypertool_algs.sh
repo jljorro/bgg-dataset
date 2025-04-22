daa55928810dc55e8b5a52f10bdcb582d89c541e
Jose Luis Jorro-Aragoneses <jose.jorro@uam.es>
2025-04-22 23:37:40 +0200

    Removed first test

--- test/hypertool_algs.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-python itemknn_hyper.py --model='ItemKNN' --dataset='bgg_24' --config_files=['deepfm_config2.yml'] --tool=Hyperopt
--- test/run.py
+++
@@ -1,3 +0,0 @@
-from recbole.quick_start import run_recbole
-
-run_recbole(dataset='bgg_24', model='ItemKNN', config_file_list=['deepfm_config2.yml'])
