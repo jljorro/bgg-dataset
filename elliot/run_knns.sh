4114fb30f54ee8a40112ce4caebf8fb4d3e8ece9
José L. Jorro-Aragoneses <jose.jorro@uam.es>
2025-05-04 08:46:21 +0200

    Configured elliot scripts to be executed in ceres2

--- elliot/run_knns.sh
+++ elliot/run_knns.sh
@@ -4,8 +4,8 @@ SCRIPTS=("ItemKNN" "UserKNN")
 
 for script in ${SCRIPTS[@]}; do
     echo "Ejecutndo script ${script}..."
-	python3 "run_${script}.py"
-	echo "${script} finalizado!"
+    python3 "run_${script}.py" > logs/$script.log 2>&1 &
+    echo "${script} finalizado!"
 done
 
 wait
